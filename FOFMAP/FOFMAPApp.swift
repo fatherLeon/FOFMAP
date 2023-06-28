e9ff947428a6dce29ae787a6232834664d823af6
minsu3007 <minsu3007@gmail.com>
2023-06-28 14:25:33 +0900

    Initial Commit

---
+++ FOFMAP/FOFMAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FOFMAPApp.swift
+//  FOFMAP
+//
+//  Created by 강민수 on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct FOFMAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
