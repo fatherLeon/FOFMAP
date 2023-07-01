1d47bad4567147b14f6315acb7313878665c0f5f
minsu3007 <minsu3007@gmail.com>
2023-07-01 17:43:45 +0900

    feat: generate MatchInfoCategory DTO

---
+++ FOFMAP/Data/Network/DTO/MatchInfoCategory/MatchAllRecord.swift
@@ -0,0 +1,21 @@
+//
+//  MatchAllRecord.swift
+//  FOFMAP
+//
+//  Created by 강민수 on 2023/07/01.
+//
+
+import Foundation
+
+/*
+ [
+     "5d25fa0fea00678d61010bd7",
+     "5d25fa336325f381bc55fad5",
+     "5d25fa380758fe690757bd81",
+     "5d25fa3c13a97367e6e4ae6e",
+     "5d25fa6bea0067ee1b0111f9",
+     "5d25fa720758fe4b9b57c187"
+ ]
+ */
+
+typealias MatchAllRecord = [String]
