441c6d8acdecc821227f79acb4655d96d8829b3e
minsu3007 <minsu3007@gmail.com>
2023-07-01 20:12:19 +0900

    test: userMatchesURL && userTradeHistory testcase

--- FOFMAPTests/ContentTypeTests.swift
+++ FOFMAPTests/ContentTypeTests.swift
@@ -12,15 +12,11 @@ import XCTest
 final class ContentTypeTests: XCTestCase {
     
     private let userId = "27ee6f0418d480e60743174b"
-    private var sut: ContentType!
 
-    override func tearDownWithError() throws {
-        sut = nil
-    }
-
+    // MARK: 유저정보 테스트
     func test_checkUserInfoURL() throws {
         // given
-        sut = ContentType.userInfo(nickname: "Chelsea")
+        let sut = ContentType.userInfo(nickname: "Chelsea")
         
         let expectation = "https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=Chelsea"
         
@@ -33,7 +29,7 @@ final class ContentTypeTests: XCTestCase {
     
     func test_checkUserMaxGradeURL() throws {
         // given
-        sut = ContentType.userMaxGrade(id: userId)
+        let sut = ContentType.userMaxGrade(id: userId)
         
         let expectation = "https://api.nexon.co.kr/fifaonline4/v1.0/users/27ee6f0418d480e60743174b/maxdivision"
         
@@ -44,7 +40,33 @@ final class ContentTypeTests: XCTestCase {
         XCTAssertEqual(expectation, result)
     }
     
-    func test_checkUserMatchesURL() throws {
+    func test_checkUserMatchesURL_and_overLimitValue100() throws {
+        // given
+        let sut = ContentType.userMatches(id: userId, matchType: 50, offset: 0, limit: 100)
+        let overSut = ContentType.userMatches(id: userId, matchType: 50, offset: 0, limit: 120)
+        let expectation = "https://api.nexon.co.kr/fifaonline4/v1.0/users/27ee6f0418d480e60743174b/matches?matchtype=50&offset=0&limit=100"
         
+        // when
+        let result = sut.url!.absoluteString
+        let overLimitSutResult = overSut.url!.absoluteString
+        
+        // then
+        XCTAssertEqual(expectation, result)
+        XCTAssertEqual(expectation, overLimitSutResult)
+    }
+    
+    func test_checkUserTradeHistory_and_overLimitValue100() throws {
+        // given
+        let sut = ContentType.userTradeHistory(id: userId, tradeType: .buy, offset: 0, limit: 100)
+        let overSut = ContentType.userTradeHistory(id: userId, tradeType: .buy, offset: 0, limit: 150)
+        let expectation = "https://api.nexon.co.kr/fifaonline4/v1.0/users/27ee6f0418d480e60743174b/markets?tradetype=buy&offset=0&limit=100"
+        
+        // when
+        let result = sut.url!.absoluteString
+        let overLimitSutResult = overSut.url!.absoluteString
+        
+        // then
+        XCTAssertEqual(expectation, result)
+        XCTAssertEqual(expectation, overLimitSutResult)
     }
 }
