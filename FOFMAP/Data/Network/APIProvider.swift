a9f5f8c33df55592c4517ec60515c8ef519adf98
minsu3007 <minsu3007@gmail.com>
2023-07-02 15:55:48 +0900

    refactor: add Cache in APIProvider

--- FOFMAP/Data/Network/APIProvider.swift
+++ FOFMAP/Data/Network/APIProvider.swift
@@ -16,28 +16,51 @@ struct APIProvider {
         self.cacheManager = cacheManager
     }
     
+    func receiveData<T: Decodable>(url: URL, by type: T.Type) async throws -> T {
+        let request = try generateRequest(by: url)
+        let parsingModel = ParsingModel()
+
+        guard let (data, _) = cacheManager.getCachedResponse(for: request) else {
+            let data = try await connectingNetwork(by: request)
+            let parsedData = try parsingModel.toJson(data: data, by: type)
+            
+            return parsedData
+        }
+        
+        let parsedData = try parsingModel.toJson(data: data, by: type)
+            
+        return parsedData
+    }
+    
+    func receiveImage(by url: URL) async throws -> UIImage {
+        let request = try generateRequest(by: url)
+        let parsingModel = ParsingModel()
+        
+        guard let (data, _) = cacheManager.getCachedResponse(for: request) else {
+            let data = try await connectingNetwork(by: request)
+            let image = try parsingModel.toImage(data: data)
+            
+            return image
+        }
+        
+        let image = try parsingModel.toImage(data: data)
+        
+        return image
+    }
+    
+    private func connectingNetwork(by request: URLRequest) async throws -> Data {
+        let (data, response) = try await networkManager.request(with: request)
+        
+        cacheManager.storeCache(response: response, data: data, in: request)
+        
+        return data
+    }
+    
     private func generateRequest(by url: URL) throws -> URLRequest {
         var request = URLRequest(url: url)
         
         request.addValue("", forHTTPHeaderField: "Authorization")
         
         return request
-    }
-    
-    func receiveData<T: Decodable>(url: URL, by type: T.Type) async throws -> T {
-        let request = try generateRequest(by: url)
-        let data = try await networkManager.request(with: request)
-        let parsedData = try ParsingModel().toJson(data: data, by: type)
-        
-        
-        return parsedData
-    }
-    
-    func receiveImage(by url: URL) async throws -> UIImage {
-        let request = try generateRequest(by: url)
-        let data = try await networkManager.request(with: request)
-        let image = try ParsingModel().toImage(data: data)
-        
-        return image
     }
 }
