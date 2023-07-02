c4ffe4735481283d9cba57ab6be982edab80a37e
minsu3007 <minsu3007@gmail.com>
2023-07-02 18:36:50 +0900

    test: implement success networkManager testcase

--- FOFMAPTests/Tests/NetworkManagerTests.swift
+++ FOFMAPTests/Tests/NetworkManagerTests.swift
@@ -5,31 +5,45 @@
 //  Created by 강민수 on 2023/07/02.
 //
 
+@testable
+import FOFMAP
 import XCTest
 
 final class NetworkManagerTests: XCTestCase {
+    
+    var sut: NetworkManager!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [MockURLProtocol.self]
+        let session = URLSession(configuration: config)
+        
+        sut = NetworkManager(session: session)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_isSuccessResponseByStatusCode200_and_StubData_userTradeHistoryData() async throws {
+        // given
+        MockURLProtocol.requestHandler = { request in
+            let response = HTTPURLResponse(url: URL(string: "www.naver.com")!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            let stubData = StubJsonData.userTradeHistoryData!
+            
+            return (response, stubData)
         }
+        
+        let request = URLRequest(url: URL(string: "www.naver.com")!)
+        let expectationData = StubJsonData.userTradeHistoryData!
+        let expectationStatusCode = 200
+        
+        // when
+        let (resultData, response) = try await sut.request(with: request)
+        let httpResponse = response as! HTTPURLResponse
+        
+        // then
+        XCTAssertEqual(expectationData, resultData)
+        XCTAssertEqual(expectationStatusCode, httpResponse.statusCode)
     }
-
 }
