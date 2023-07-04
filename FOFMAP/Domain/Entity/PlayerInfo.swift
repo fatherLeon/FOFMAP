0d42cfbe250e1838a6828de4d1f022b37b09259a
minsu3007 <minsu3007@gmail.com>
2023-07-04 15:54:54 +0900

    feat: PlayerInfo object

---
+++ FOFMAP/Domain/Entity/PlayerInfo.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerInfo.swift
+//  FOFMAP
+//
+//  Created by 강민수 on 2023/07/04.
+//
+
+import UIKit
+
+struct PlayerInfo {
+    let name: String
+    let seasonImg: UIImage
+    let img: UIImage
+}
---
+++ FOFMAP/Domain/UseCase/MostUsedPlayerUseCase.swift
@@ -0,0 +1,76 @@
+//
+//  NetworkingUseCase.swift
+//  FOFMAP
+//
+//  Created by 강민수 on 2023/07/04.
+//
+
+import UIKit
+
+final class MostUsedPlayerUseCase {
+    private var players: [Int: Int] = [:]
+    private let provider = APIProvider()
+    
+    func getMostUsedPlayer() async throws {
+        let matches = try await getAllMatches(.matchAllRecord(matchType: 50, offset: 0, limit: 100))
+        
+        for id in matches {
+            var players = try await getMatchDescPlayer(.matchDesc(matchId: id))
+            
+            players.forEach { player in
+                if self.players[player.spID] == nil {
+                    self.players[player.spID] = 1
+                } else {
+                    self.players[player.spID]? += 1
+                }
+            }
+        }
+        
+        let playersID = players.sorted { $0.value > $1.value }
+    }
+    
+    private func getAllMatches(_ contentType: ContentType) async throws -> MatchAllRecord {
+        guard let url = contentType.url else {
+            throw NetworkError.urlError
+        }
+        
+        let matchesID = try await provider.receiveData(url: url, by: MatchAllRecord.self)
+        
+        return matchesID
+    }
+    
+    private func getMatchDescPlayer(_ contentType: ContentType) async throws -> [Player] {
+        guard let url = contentType.url else {
+            throw NetworkError.urlError
+        }
+        
+        var result: [Player] = []
+        let matchDesc = try await provider.receiveData(url: url, by: MatchDesc.self)
+        
+        matchDesc.matchInfo.forEach { matchInfo in
+            result += matchInfo.player
+        }
+        
+        return result
+    }
+    
+    private func getPlayerActionImage(by spid: Int) async throws -> UIImage {
+        guard let url = ContentType.metaPlayerActionshotImageBySpid(spid: spid).url else {
+            throw NetworkError.urlError
+        }
+        
+        let playerActionImage = try await provider.receiveImage(by: url)
+        
+        return playerActionImage
+    }
+    
+    private func getPlayerImage(by spid: Int) async throws -> UIImage {
+        guard let url = ContentType.metaPlayerImageBySpid(spid: spid).url else {
+            throw NetworkError.urlError
+        }
+        
+        let playerImage = try await provider.receiveImage(by: url)
+        
+        return playerImage
+    }
+}
