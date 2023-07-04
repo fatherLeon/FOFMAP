69a495f274ee43d1e417fe06c8cf51e65822039b
minsu3007 <minsu3007@gmail.com>
2023-07-04 18:16:51 +0900

    feat: implement MostUsedPlayer get method

--- FOFMAP/Domain/UseCase/MostUsedPlayerUseCase.swift
+++ FOFMAP/Domain/UseCase/MostUsedPlayerUseCase.swift
@@ -9,11 +9,40 @@ import Foundation
 
 final class MostUsedPlayerUseCase {
     private let networkingUseCase = FetchNetworkUseCase()
+    private var usedPlayers: [Int: Int] = [:]
     
-    func execute() {
+    var mostUsedPlayers: [PlayerInfo] = []
+    
+    func execute() -> [PlayerInfo] {
         Task {
             let matcheIds = try await networkingUseCase.getAllMatches(50, offset: 0, limit: 100, orderBy: .desc)
             
+            for id in matcheIds {
+                let players = try await networkingUseCase.getMatchDescAllPlayers(matchId: id)
+                
+                players.forEach { player in
+                    if usedPlayers[player.spID] != nil {
+                        usedPlayers[player.spID]? += 1
+                        return
+                    }
+                    
+                    usedPlayers[player.spID] = 1
+                }
+            }
+            
+            for (id, _) in usedPlayers {
+                if mostUsedPlayers.count > 10 {
+                    break
+                }
+                
+                let name = try await networkingUseCase.getPlayerName(by: id)
+                let actionImage = try await networkingUseCase.getPlayerActionImage(by: id)
+                let seasonImage = try await networkingUseCase.getSeasonImage(by: id)
+                
+                mostUsedPlayers.append(PlayerInfo(name: name, seasonImg: seasonImage, img: actionImage))
+            }
         }
+        
+        return mostUsedPlayers
     }
 }
