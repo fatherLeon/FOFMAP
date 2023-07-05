04e179e66f0478f5523a7d21c31eae56d74b7aad
minsu3007 <minsu3007@gmail.com>
2023-07-05 18:46:15 +0900

    feat: write NetworkError description

--- FOFMAP/Data/Network/Entity/NetworkError.swift
+++ FOFMAP/Data/Network/Entity/NetworkError.swift
@@ -13,4 +13,19 @@ enum NetworkError: LocalizedError, Equatable {
     case responseError(statusCode: Int)
     case invalidData
     case incorrectType
+    
+    var errorDescription: String? {
+        switch self {
+        case .httpError:
+            return "네트워크 연결 오류입니다."
+        case .incorrectType:
+            return "올바르지 않은 데이터 타입입니다."
+        case .invalidData:
+            return "올바리즈 않은 데이터입니다."
+        case .responseError(let statusCode):
+            return "서버 오류입니다.\(statusCode)"
+        case .urlError:
+            return "잘못된 형식의 URL입니다."
+        }
+    }
 }
