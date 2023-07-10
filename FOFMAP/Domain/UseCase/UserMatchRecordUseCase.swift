a68144a64e3010de6c2ff2eb827c0f8957fa050a
minsu3007 <minsu3007@gmail.com>
2023-07-10 12:12:18 +0900

    feat: implement getUserInfo method In UserMatchRecordUseCase

--- FOFMAP/Domain/UseCase/UserMatchRecordUseCase.swift
+++ FOFMAP/Domain/UseCase/UserMatchRecordUseCase.swift
@@ -11,13 +11,21 @@ struct UserMatchRecordUseCase: DetailFetchable {
     typealias T = [MatchDesc]
     
     private let offerableUseCase: Offerable
+    private let nickname: String
     
-    init(offerableUseCase: Offerable = FetchUseCase()) {
+    init(nickname: String, offerableUseCase: Offerable = FetchUseCase()) {
+        self.nickname = nickname
         self.offerableUseCase = offerableUseCase
     }
     
     func execute() async throws -> [MatchDesc] {
         return []
+    }
+    
+    private func getUserInfo(name: String) async throws -> UserInfo {
+        let (id, level) = try await offerableUseCase.getUserInfo(by: name)
+        
+        return UserInfo(id: id, nickname: name, level: level)
     }
     
     private func getUserMatchIds(userId: String, matchType: Int, offset: Int, limit: Int) async throws -> UserMatches {
