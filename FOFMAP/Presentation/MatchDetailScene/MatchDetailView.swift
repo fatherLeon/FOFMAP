0a4a290ecbe3c80a395fa3e81ababbeb5db00639
minsu3007 <minsu3007@gmail.com>
2023-07-14 14:37:12 +0900

    feat: implement Picker in MatchDetailView

---
+++ FOFMAP/Presentation/MatchDetailScene/MatchDetailView.swift
@@ -0,0 +1,42 @@
+//
+//  MatchDetailView.swift
+//  FOFMAP
+//
+//  Created by 강민수 on 2023/07/14.
+//
+
+import SwiftUI
+
+enum UserPick: String, CaseIterable, Identifiable {
+    case user = "유저"
+    case enemy = "상대"
+    
+    var id: Self {
+        return self
+    }
+}
+
+struct MatchDetailView: View {
+    
+    private var matchDesc: MatchDesc?
+    @State var pick = UserPick.user
+    
+    var body: some View {
+        VStack {
+            Picker("UserState", selection: $pick) {
+                ForEach(UserPick.allCases) { picked in
+                    Text(picked.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
+            
+            GroundView()
+        }
+    }
+}
+
+struct MatchDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchDetailView()
+    }
+}
