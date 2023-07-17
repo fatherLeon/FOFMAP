d76f0fd1b0b0f7ce5bdb42573facfc89252b3765
minsu3007 <minsu3007@gmail.com>
2023-07-17 13:14:05 +0900

    feat: DetailPlayerUseCase

--- FOFMAP/Domain/UseCase/DetailPlayerUseCase.swift
+++ FOFMAP/Domain/UseCase/DetailPlayerUseCase.swift
@@ -5,19 +5,40 @@
 //  Created by 강민수 on 2023/07/17.
 //
 
-import Foundation
+import UIKit
 
 struct DetailPlayerUseCase: DetailFetchable {
     
-    typealias T = [PlayerInfo]
+    typealias T = PlayerInfo?
     
     private let offerableUseCase: Offerable
+    private let targetPlayer: Player
     
-    init(offerableUseCase: Offerable = FetchUseCase()) {
+    init(targetPlayer: Player, offerableUseCase: Offerable = FetchUseCase()) {
+        self.targetPlayer = targetPlayer
         self.offerableUseCase = offerableUseCase
     }
     
-    func execute() async throws -> [PlayerInfo] {
-        throw NetworkError.httpError
+    func execute() async throws -> PlayerInfo? {
+        guard let name = try? await getPlayerName(),
+              let seasonImg = try? await getPlayerSeasonImage(),
+              let playerImg = try? await getPlayerImage(),
+              let position = PlayerSection.getPosition(by: targetPlayer.spPosition) else {
+            return nil
+        }
+        
+        return PlayerInfo(id: targetPlayer.spID, name: name, seasonImg: seasonImg, img: playerImg, positionId: targetPlayer.spPosition, position: position)
+    }
+    
+    private func getPlayerImage() async throws -> UIImage {
+        return try await offerableUseCase.getPlayerActionImage(by: targetPlayer.spID)
+    }
+    
+    private func getPlayerSeasonImage() async throws -> UIImage {
+        return try await offerableUseCase.getSeasonImage(by: targetPlayer.spID)
+    }
+    
+    private func getPlayerName() async throws -> String {
+        return try await offerableUseCase.getPlayerName(by: targetPlayer.spID)
     }
 }
