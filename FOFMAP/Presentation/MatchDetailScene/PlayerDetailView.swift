ea8eb531ea399d734e21fbed12aa5872e55c3a33
minsu3007 <minsu3007@gmail.com>
2023-07-18 18:45:52 +0900

    docs: create PlayerDetailView file

---
+++ FOFMAP/Presentation/MatchDetailScene/PlayerDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerDetailView.swift
+//  FOFMAP
+//
+//  Created by 강민수 on 2023/07/18.
+//
+
+import SwiftUI
+
+struct PlayerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerDetailView()
+    }
+}
