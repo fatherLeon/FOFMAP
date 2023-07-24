58918f6cde3e3b121f9045da1f2dd5f32e099fc0
minsu3007 <minsu3007@gmail.com>
2023-07-24 18:22:40 +0900

    test: refactoring tests

--- FOFMAPTests/TestDoubles/Domain/UseCase/MockFetchUseCase.swift
+++ FOFMAPTests/TestDoubles/Domain/UseCase/MockFetchUseCase.swift
@@ -11,18 +11,18 @@ import UIKit
 
 struct MockFetchUseCase: Offerable {
     static var samplePlayers: [PlayerInfo] = [
-        PlayerInfo(id: 0, name: "Z", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 1, name: "a", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 2, name: "b", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 3, name: "c", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 4, name: "d", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 5, name: "e", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 6, name: "f", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 7, name: "g", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 8, name: "h", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 9, name: "i", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 10, name: "j", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker),
-        PlayerInfo(id: 11, name: "k", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(named: "SamplePlayerImage")!, position: .striker)
+        PlayerInfo(id: 0, name: "Z", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 10, position: .striker),
+        PlayerInfo(id: 1, name: "a", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 10, position: .striker),
+        PlayerInfo(id: 2, name: "b", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 10, position: .striker),
+        PlayerInfo(id: 3, name: "c", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 10, position: .striker),
+        PlayerInfo(id: 4, name: "d", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 10, position: .striker),
+        PlayerInfo(id: 5, name: "e", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 1, position: .striker),
+        PlayerInfo(id: 6, name: "f", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 2, position: .striker),
+        PlayerInfo(id: 7, name: "g", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 3, position: .striker),
+        PlayerInfo(id: 8, name: "h", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 4, position: .striker),
+        PlayerInfo(id: 9, name: "i", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 7, position: .striker),
+        PlayerInfo(id: 10, name: "j", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 5, position: .striker),
+        PlayerInfo(id: 11, name: "k", seasonImg: UIImage(systemName: "star.fill")!, img: UIImage(systemName: "person.fill")!, positionId: 6, position: .striker)
     ]
     
     func getPlayerName(by spid: Int) async throws -> String {
@@ -55,5 +55,25 @@ struct MockFetchUseCase: Offerable {
         }
         
         return players
+    }
+    
+    func getUserMatchIds(accessId: String, matchType: Int, offset: Int, limit: Int) async throws -> UserMatches {
+        return ["abc", "123", "456"]
+    }
+
+    func getUserBestGrade(accessId: String) async throws -> UserGrade {
+        return [GradeCategory(matchType: 50, division: 12, achievementDate: "2023-04-12")]
+    }
+
+    func getMatchDesc(matchId: String) async throws -> MatchDesc {
+        return MatchDesc(matchID: "1234", matchDate: "2023-04-12", matchType: 50, matchInfo: [])
+    }
+
+    func getPlayerImage(by spid: Int) async throws -> UIImage {
+        return UIImage(systemName: "person.fill")!
+    }
+
+    func getMetaDivisionGrade(gradeId: Int) async throws -> String {
+        return "슈퍼챔피언스"
     }
 }
