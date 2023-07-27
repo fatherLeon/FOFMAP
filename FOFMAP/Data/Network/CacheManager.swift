b2f8925fcc4d25c03d070054e462600a0c02507e
minsu3007 <minsu3007@gmail.com>
2023-07-27 15:12:49 +0900

    feat: add DispatchSemaphore in CacheManager

--- FOFMAP/Data/Network/CacheManager.swift
+++ FOFMAP/Data/Network/CacheManager.swift
@@ -9,18 +9,25 @@ import Foundation
 
 final class CacheManager: CacheStorable {
     private let urlCache: URLCache
+    private let semaphore = DispatchSemaphore(value: 1)
     
-    init(urlCache: URLCache = URLCache(memoryCapacity: 0, diskCapacity: 10 * 1024 * 1024)) {
+    init(urlCache: URLCache = .shared) {
         self.urlCache = urlCache
     }
     
     func storeCache(response: URLResponse, data: Data, in request: URLRequest) {
+        semaphore.signal()
+        
         let cachedResponse = CachedURLResponse(response: response, data: data)
         
         urlCache.storeCachedResponse(cachedResponse, for: request)
+        
+        semaphore.wait()
     }
     
     func getCachedResponse(for request: URLRequest) -> (Data, URLResponse)? {
+        semaphore.signal()
+        
         guard let cache = urlCache.cachedResponse(for: request) else {
             return nil
         }
@@ -28,14 +35,24 @@ final class CacheManager: CacheStorable {
         let data = cache.data
         let response = cache.response
         
+        semaphore.wait()
+        
         return (data, response)
     }
     
     func removeCache(for request: URLRequest) {
+        semaphore.signal()
+        
         urlCache.removeCachedResponse(for: request)
+        
+        semaphore.wait()
     }
     
     func removeAllCache() {
+        semaphore.signal()
+        
         urlCache.removeAllCachedResponses()
+        
+        semaphore.wait()
     }
 }
